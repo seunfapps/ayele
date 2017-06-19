e163be505d0edef3e4fbff044f38ae4619484d23
Ikechi Michael <mykehell123@gmail.com>
2017-06-19 15:09:15 +0100

    slackbot api is in stable state

--- web-api/ayele.bot.js
+++ web-api/ayele.bot.js
@@ -12,4 +12,24 @@ const AyeleBot = function Constructor(settings) {
     this.user = null
 }
 util.inherits(AyeleBot, Bot)
+
+AyeleBot.prototype._onMessage = function (message) {
+    if (this._isChatMessage(message) && 
+        this._isChannelConversation(message) && 
+        !this._isFromAyeleBot(message) && 
+        this._mentionsAyeleBot(message)) {
+            
+        }
+}
+
+AyeleBot.prototype._isChatMessage = (message) => (message && message.type === 'message' && Boolean(message.text))
+AyeleBot.prototype._isChannelConversation = (message) => (message && typeof(message.channel) === 'string' && message.channel[0] === 'C')
+AyeleBot.prototype._isFromAyeleBot = function (message) { return (message && message.user == this.user.id); }
+AyeleBot.prototype._mentionsAyeleBot = function (message) { return (message && message.text.toLowerCase().) }
+
+AyeleBot.prototype.run = function () {
+    AyeleBot.super_.call(this, this.settings)
+    this.on("start", this._onStart)
+    this.on("message", this._onMessage)
+}
 module.exports = AyeleBot
--- web-api/controllers/bot.controller.js
+++ web-api/controllers/bot.controller.js
@@ -1,6 +1,9 @@
 const config = require("../config.json")
 const sounds = require("../data/sounds.json")
+const request = require("request")
 const XString = require("../prototypes/string.prototype")
+const base64 = require("base64-stream")
+let MemoryStream = require("memory-stream")
 let BotResponse = function (text, img, sound) {
     return {
         response_type: 'in_channel',
@@ -21,6 +24,27 @@ exports.get = function (req, res) {
 exports.post = function (req, res) {
     let sound = sounds.sort((soundA, soundB) => soundB.name._looksLikeItContains(req.body.text) - soundA.name._looksLikeItContains(req.body.text))[0];
     if (sound) {
+        /*let ms = new MemoryStream();
+        ms.on("finish", function () {
+            const FormData = require("form-data")
+            let form = new FormData();
+            form.append("token", config.slackAccessToken);
+            form.append("file", ms.toBuffer(), { filename: sound.url.substring(sound.url.indexOf('/') + 1) });
+            form.append("filename", sound.url.substring(sound.url.indexOf('/') + 1));
+            form.append("filetype", "auto");
+            form.append("title", sound.name);
+            form.append("channels", req.body.channel_name);
+            form.append("initial_comment", "Reply from Ayele!");
+            form.submit("https://slack.com/api/files.upload", function (err, data) {
+                if (err) throw err;
+                console.dir(data.res);
+            })
+        })
+        request.get({ url: config.repoRootUrl + sound.url, encoding: null }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
+        }).pipe(ms);*/
         return res.json(new BotResponse(sound.name, sound.img, sound.url));
     }
     else return res.send(null);
