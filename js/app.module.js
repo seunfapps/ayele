47f7f9339182e3095d2dc91a2365d8fb257722a0
Ikechi Michael <mykehell123@gmail.com>
2017-06-19 16:03:12 +0100

    added ui.router and $templateCache for partials

--- js/app.module.js
+++ js/app.module.js
@@ -1,1 +1,30 @@
-var myApp = angular.module('myApp', []);
+/// <reference path="config.js" />
+
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('featured-memes', {
+            url: '/',
+            templateUrl: rootUrl('partials/featured-memes.html')
+        })
+        .state('my-memes', {
+            url: '/my-memes',
+            templateUrl: rootUrl('partials/my-memes.html')
+        });
+
+});
+
+myApp.run(function ($templateCache, $http) {
+    var templates = [
+        rootUrl('partials/featured-memes.html'),
+        rootUrl('partials/my-memes.html')
+    ];
+    templates.forEach(function (templateUrl) {
+        $http.get(templateUrl).then(function (response) {
+            $templateCache.put(templateUrl, response.data);
+        }).catch(errorHandler);
+    });
+})
